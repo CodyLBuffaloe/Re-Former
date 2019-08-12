6ebd7635ceee7af75afcb8af68907d01178e25a9
Cody Laura Buffaloe <codylbuffaloe@gmail.com>
2019-08-12 15:46:59 -0700

    Adds validation for presence to model User params

--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name
-  validates :email
-  validates :password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
