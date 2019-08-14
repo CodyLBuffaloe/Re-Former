fdffac14d0f97a84637342f97e060920acfa2423
Cody Laura Buffaloe <codylbuffaloe@gmail.com>
2019-08-13 17:19:18 -0700

    Adds auth_token for new.html and fleshes out UsersController

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(username: params[:name], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+      end
+    end
 
   end
 end
