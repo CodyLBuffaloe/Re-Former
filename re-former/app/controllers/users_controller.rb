4e017add3aab71b7430bd2a5a55f500da1ac15ed
Cody Laura Buffaloe <codylbuffaloe@gmail.com>
2019-08-13 17:45:49 -0700

    Secures user params behind private permit and require method #user_params

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -4,14 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:name], email: params[:email], password: params[:password])
-
+   # @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
       render :new
-      end
     end
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
