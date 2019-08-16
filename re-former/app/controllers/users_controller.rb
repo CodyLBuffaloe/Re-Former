1b1f30955b6e1e7ee38eb274f01105736fe66040
Cody Laura Buffaloe <codylbuffaloe@gmail.com>
2019-08-16 12:10:52 -0700

    Fixes User#update to correctly edit users

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
 
   end
+
 
   def create
     @user = User.new(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password])
@@ -14,10 +16,26 @@ class UsersController < ApplicationController
     end
 
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.name = user_params[:name]
+    @user.email = user_params[:email]
+    @user.password = user_params[:password]
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
